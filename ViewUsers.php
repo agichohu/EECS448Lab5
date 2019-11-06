4483b5d031bfd2573b6af6c20fa143329cd28e4f
Antonette Gichohu <amgichohu@gmail.com>
2019-11-05 20:03:37 -0600

    View users added

---
+++ ViewUsers.php
@@ -0,0 +1,29 @@
+<?php 
+
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "gichohu", "milksauce72", "gichohu");
+
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+        echo"Connection error<br>";
+        exit();
+    }
+
+    $query = $mysqli->query("SELECT user_id FROM Users");
+
+    if($query)
+    {
+        echo "here is the table: <table>";
+
+        while($row = $query->fetch_assoc())
+        {
+            echo "<tr><td>" . $row['user_id'] ."</td></tr>";
+        }
+        echo"</table>";
+    }
+    else
+    {
+        echo "Something went wrong <br>";
+    }
+
+
+?>
